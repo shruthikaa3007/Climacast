5364909c4e26891ef580e07f24c547482dd9629a
shruthikaa3007 <139707646+shruthikaa3007@users.noreply.github.com>
2024-09-02 22:57:40 +0530

    Add files via upload

---
+++ assets/js/module.js
@@ -0,0 +1,91 @@
+'use strict';
+
+export const weekDayNames =[
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat"
+];
+export const monthNames =[
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+];
+/*@param {number} dateUnix Unix date in seconds
+@param {*} timezone time
+@returns {string}*/
+
+export const getDate = function(dateUnix, timezone){
+    const date= new Date((dateUnix + timezone)* 1000);
+    const weekdayName = weekDayNames[date.getUTCDay()];
+    const monthName = monthNames[date.getUTCMonth()];
+
+    return `${weekdayName} ${date.getUTCDate()}, ${monthName}`;
+
+}
+
+
+export const getTime = function(timeUnix, timezone) {
+    const date = new Date((timeUnix + timezone) * 1000);
+    const hours = date.getUTCHours();
+    const minutes = date.getUTCMinutes();
+    const period = hours >= 12 ? "PM" : "AM";
+
+    return `${hours % 12 || 12}:${minutes} ${period}`;
+
+}
+
+export const getHours = function(timeUnix, timezone) {
+    const date = new Date((timeUnix + timezone) * 1000);
+    const hours = date.getUTCHours();
+    const period = hours >= 12 ? "PM" : "AM";
+
+    return `${hours % 12 || 12} ${period}`;  
+}
+
+export const mps_to_kmh = mps => {
+    const mph = mps * 3600;
+    return mph / 1000;
+}
+
+export const aqiText = {
+    1: {
+        level: "Good",
+        message: "Air quality is considered satisfactory, and air pollution poses little to no risk"
+    },
+    2: {
+        level: "Fair",
+        message: "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution."
+
+    },
+
+    3: {
+        level: "Moderate",
+        message: "Members of sensitive groups may experience health effects. The general public is not likely to be affected."
+
+    },
+
+    4: {
+        level: "Poor",
+        message: "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects."
+
+    },
+
+    5: {
+        level: "Very Poor",
+        message: "Health warnings of emergency conditions. The entire population is more likely to be affected"
+
+    }
+}
